285d6d28ff49929a4af4432ea6f635890f070d09
ide Shouhi <shouhi0922@icloud.com>
2021-03-06 20:55:06 +0900

    contract/policy

---
+++ layouts/contract.vue
@@ -0,0 +1,15 @@
+<template lang="pug">
+  v-app
+    v-main
+      Nuxt
+</template>
+
+<script>
+export default {
+  components: {},
+  data() {
+    return {}
+  },
+}
+</script>
+<style scoped></style>
